6a5c19eb2383252b744652cbddb096c7362d972c
unknown <erskine.duenas@gmail.com>
2022-09-12 10:17:53 +0800

    update

--- potion.py
+++ potion.py
@@ -1,7 +1,13 @@
 import random
 
+health = 50
 
-potion_health = random.randint(10,100)  
+difficulty = 1   #1 for easy, 2 for intermediate, 3 for hard
 
-print("Hello, World")
+potion_health = random.randint(25,50) / difficulty
+
+health = health + potion_health
+
+print(health) 
+
 
